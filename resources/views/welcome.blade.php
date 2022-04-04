ce5a1708c39ed03dfa3ab5f15df82b13078a8e70
chrisgardner402 <chojh3308@gmail.com>
2022-04-05 00:11:35 +0900

    blade layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout')
 
-        <title>Laravel</title>
+@section('title')
+    Welcome
+@endsection
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    </head>
-    <body>
-        Laravel
-    </body>
-</html>
+@section('content')
+    Welcome
+@endsection
