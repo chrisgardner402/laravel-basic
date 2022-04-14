a4c7b01b2c4a8caf03bf492d74ea2a8f92a9e01e
chrisgardner402 <chojh3308@gmail.com>
2022-04-14 22:01:28 +0900

    submit task form

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,17 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index()
     {
-        return view('tasks.index');
+        $tasks = Task::all();
+        return view('tasks.index', [
+            'tasks' => $tasks
+        ]);
     }
 
     public function create()
     {
         return view('tasks.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->input('title');
+        $request->input('body');
+        $task = Task::create([
+            'title' => $request->input('title'),
+            'body' => $request->input('body')
+        ]);
+        return redirect('/tasks');
     }
 }
--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,12 +3,15 @@
 @section('title', 'Create Task')
 
 @section('content')
-    <h1 class="font-bold text-3xl">Create Task</h1>
-    <form action="/tasks" method="POST">
-        <label class="block" for="title">Title</label>
-        <input class="border border-gray-800" type="text" name="title" id="title">
-        <label class="block" for="body">Body</label>
-        <textarea class="border border-gray-800" name="body" id="body" cols="30" rows="10"></textarea>
-        <button class="block bg-blue-100">Submit</button>
-    </form>
+    <div class="px-64">
+        <h1 class="font-bold text-3xl">Create Task</h1>
+        <form action="/tasks" method="POST">
+            @csrf
+            <label class="block" for="title">Title</label>
+            <input class="border border-gray-800 w-full" type="text" name="title" id="title">
+            <label class="block" for="body">Body</label>
+            <textarea class="border border-gray-800 w-full" name="body" id="body" cols="30" rows="10"></textarea>
+            <button class="block bg-blue-600 text-white px-4 py-2 float-right">Submit</button>
+        </form>
+    </div>
 @endsection
